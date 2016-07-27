94969e206222660d2e462e64e348d1aaa871112f
Michael Busta <michaelbusta@Michaels-MacBook-Pro.local>
2016-07-27 10:01:04 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = React.createClass({
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  };
+});
+
+export default App;
